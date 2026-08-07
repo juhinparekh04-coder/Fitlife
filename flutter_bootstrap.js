63fcdb113f0a4610574f00f88872772ab0b6da48
juhinparekh04-coder <juhinparekh04@gmail.com>
2026-08-07 10:43:13 +0000

    Deploying to gh-pages from @ juhinparekh04-coder/Fitlife@81e591602f137780c15e7e13be0c98260d27fdaa 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1865992320" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1018473140" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
